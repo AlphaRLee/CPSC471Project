c7d74b793229f201d77ebc9e44fc566a09b3219a
R Lee <alpha.r.lee.dev@gmail.com>
2019-04-08 03:47:42 -0600

    [task1.1] Built static variables for Expense model, no relationships yet.

--- app/Expense.php
+++ app/Expense.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    protected $table = 'expense'; // Custom table name required
+    // public $incrementing = false; // Control auto-incrementing of primary key
+    // protected $keyType = 'string'; // Set type of primary key
+    // public $timestamps = false; // Set whether a timestamp column exists or not
+
+    protected $fillable = [
+        'photo', 'status', 'employee_ssn'
+    ];
 }
