99242c231f909b870987d8ea18806bf346a24e6a
R Lee <alpha.r.lee.dev@gmail.com>
2019-04-10 17:28:54 -0600

    [task4.0] Got firebase database demo working.

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Database;
+
+class FirebaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/cpsc471-expensehandler-firebase-adminsdk-wltre-a1b8486a6c.json');
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__. '/cpsc471-expensehandler-firebase-adminsdk-efyjr-983436bf20.json');
+        $firebase = (new Factory)
+                ->withServiceAccount($serviceAccount)
+                ->withDatabaseUri('https://cpsc471-expensehandler.firebaseio.com/')
+                ->create();
+
+        $database = $firebase->getDatabase();
+
+        $newPost = $database
+                ->getReference('blog/posts')
+                ->push([
+                'title' => 'Laravel FireBase Tutorial' ,
+                'category' => 'Laravel'
+                ]);
+        echo '<pre>';
+        print_r($newPost->getvalue());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
