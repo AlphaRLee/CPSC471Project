60bac7bdffd5b3a302e971c212e00f9666a576fb
R Lee <alpha.r.lee.dev@gmail.com>
2019-04-10 12:08:27 -0600

    [task1.3] Created secretary model.

--- app/Secretary.php
+++ app/Secretary.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Secretary extends Model
 {
-    //
+    protected $table = 'secretary'; // Custom table name required
+    public $incrementing = false; // Control auto-incrementing of primary key
+    protected $keyType = 'string'; // Set type of primary key
+    public $timestamps = false; // Set whether a timestamp column exists or not
+
+    protected $fillable = [
+        'ssn', 'office'
+    ];
+
+    public function employee() {
+        return Employee::find($this->ssn);
+    }
 }
